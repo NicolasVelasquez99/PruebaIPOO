b647c0de05668b589576e0ce554b5ed77c80af4f
Andres981020 <mosquera.andres@correounivalle.edu.co>
2017-02-15 10:02:14 -0500

    Ya lo actualicé

--- Practica.cpp
+++ Practica.cpp
@@ -4,8 +4,10 @@ using namespace std;
 
 int main(){
 
-cout<<"Hola mundo cruel"<<endl;
-cout<<"Supongamos que....." << endl;
+	cout<<"Hola mundo cruel"<<endl;
+	cout<<"Supongamos que....." << endl;
+	cout<<"Supongamos que sé manejar github..."<<endl;
+	cout<<"No lo sé hacer pero supongamos..."<<endl;
  
 return 0;
 
